b4957e4c07d397e5ddc860dc12c9ca1d9df15fd9
M. McConnaughey <mcmxl@reagan.com>
2022-09-14 20:14:59 -0600

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,35 +1,16 @@
-class Person:
-
-    skill = 0
-    hit_points = 10
-    armor = 1
-
-    def __init__(self, name) -> None:
-        self.name = name
-
-
 class Ship:
 
     in_ship = False
     armor = 10
 
-    def __init__(self, model) -> None:
-        self.model = model
+    def __init__(self, type, name) -> None:
+        self.type = type
+        self.name = name
 
-    def enter_car(self) -> None:
+    def enter_ship(self) -> bool:
         Ship.in_ship = True
+        return Ship.in_ship
 
-    def exit_car(self) -> None:
+    def exit_ship(self) -> bool:
         Ship.in_ship = False
-
-
-def ask() :
-    enter = input('Enter car? ')
-    if enter == 'y':
-        ship.enter_car()
-        print(f'{person.name} is in the {ship.model}!')
-
-
-person = Person('Attwood')
-ship = Ship('Ship')
-ask()
+        return Ship.in_ship
