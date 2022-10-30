08a0d3b83325e86d8f78a347b8c42434c2898e28
M. McConnaughey <mcmxl@reagan.com>
2022-10-30 14:26:02 -0600

    Use Config data class from Ship module.

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 from character import Person
 from Menu import Menu
-from ship import Ship
+from ship import Ship, Config
 
 
 def main():
     """Main function."""
 
-    player = Person("Micah")
-    ship = Ship("Miner", "Venture")
+    player = Person("John Doe")
+    Config.ship_name = "Venture"
+    Config.ship_type = "miner"
+    ship = Ship()
     menu = Menu.list_choices(
         [
-            "Dock",
-            "Enter",
             "Exit",
-            "Fly",
-            "Scan",
-            "Refuel",
-            "Repair",
-            "Sell",
-            "Buy",
-            "Quit",
+            "Enter",
+            "Quit"
         ]
     )
 
     print(f"Welcome to PyEve {player.name}!")
-    print(f"Here is your first ship, a {ship.name}.")
+    print(f"Here is your first ship, a {ship.ship_type}.")
 
     enter_ship = ship.enter_ship()
 
     if enter_ship == True:
-        print(f"You are now in the {ship.name}.")
+        print(f"You are now in the {ship.ship_name}.")
 
 
 if __name__ == "__main__":
