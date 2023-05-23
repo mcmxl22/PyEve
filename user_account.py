f9c3a2b2687a86cad1d450c1f41793baa7cc59cd
M. McConnaughey <mcmxl@hotmail.com>
2023-05-23 14:25:14 -0600

    Update user_account.py

--- user_account.py
+++ user_account.py
@@ -1,71 +1,99 @@
-import getpass
-import hashlib
-import os
-import pickle
+import time
+from mining_laser import MiningLaser
 
-class UserAccount:
-    """
-    A class representing a user account with login and registration features.
-    """
-    
-    def __init__(self):
-        # Load user data from a file if it exists
-        self.user_data = {}
-        self.user_data_file = 'user_data.pkl'
-        if os.path.exists(self.user_data_file):
-            with open(self.user_data_file, 'rb') as f:
-                self.user_data = pickle.load(f)
 
-    def hash_password(self, password):
-        """Return the SHA-256 hash of the password."""
-        return hashlib.sha256(password.encode()).hexdigest()
+class Spaceship:
+    """Represent a spaceship with cargo capacity and a mining laser."""
 
-    def register_account(self):
-        """Register a new user account."""
-        username = input("\nEnter username: ")
+    def __init__(self, cargo_capacity, laser):
+        """
+        Initialize a Spaceship instance.
 
-        # Check if the username is already in use
-        if username in self.user_data:
-            print("Username is already in use.")
+        Args:
+            cargo_capacity (int): Maximum cargo capacity of the spaceship.
+            laser (MiningLaser): Mining laser equipped on the spaceship.
+        """
+        self.cargo_capacity = cargo_capacity
+        self.current_cargo = {}  # Initialize current_cargo as a dictionary
+        self.docked = True
+        self.laser = laser
+
+    def load_cargo(self, cargo):
+        """
+        Load cargo into the spaceship's cargo hold.
+
+        Args:
+            cargo (dict): Dictionary of cargo to be loaded.
+
+        Returns:
+            bool: True if the cargo is successfully loaded, False otherwise.
+        """
+        total_cargo = sum(self.current_cargo.values())
+        additional_cargo = sum(cargo.values())
+        if total_cargo + additional_cargo <= self.cargo_capacity:
+            for material, amount in cargo.items():
+                self.current_cargo[material] = self.current_cargo.get(material, 0) + amount
+            return True
+        else:
             return False
 
-        while True:
-            password = getpass.getpass("Enter password: ")
+    def unload_cargo(self):
+        """
+        Unload the spaceship's cargo.
 
-            # Check if the password meets the requirements
-            if (len(password) >= 8 and
-                    any(char.isdigit() for char in password) and
-                    any(char.isupper() for char in password) and
-                    any(char.islower() for char in password)):
-                break
-            else:
-                print("Password does not meet the requirements. Please try again.")
+        Returns:
+            dict: Dictionary of cargo that was unloaded.
+        """
+        unloaded_material = self.current_cargo
+        self.current_cargo = {}
+        return unloaded_material
 
-        # Store the username and hashed password
-        self.user_data[username] = self.hash_password(password)
+    def dock(self):
+        """
+        Dock the spaceship at the space station.
 
-        # Save the user data to a file
-        with open(self.user_data_file, 'wb') as f:
-            pickle.dump(self.user_data, f)
+        Power off the mining laser if it is powered on.
+        """
+        self.docked = True
+        print("Spaceship docked at the space station.")
+        if self.laser.is_powered:
+            self.laser.power_off()
+            print("Mining laser powered off.")
 
-        print("Account registered successfully.")
-        return True
+    def mine(self, asteroid):
+        """
+        Mine an asteroid using the spaceship's mining laser.
 
-    def login(self):
-        """Login to an existing user account."""
-        username = input("\nEnter username: ")
+        Args:
+            asteroid (Asteroid): Asteroid to be mined.
+        """
+        if self.docked:
+            print("Cannot mine while docked. Please undock first.")
+            return
 
-        # Check if the username exists
-        if username not in self.user_data:
-            print("Username does not exist.")
-            return False
+        self.laser.power_on()
 
-        password = getpass.getpass("Enter password: ")
+        while self.laser.is_powered:
+            total_cargo = sum(self.current_cargo.values())
+            if total_cargo >= self.cargo_capacity:
+                self.laser.power_off()
+                print(f"\nCargo is full. Current cargo: {self.current_cargo}")
+                return
 
-        # Check if the password is correct
-        if self.user_data[username] != self.hash_password(password):
-            print("Incorrect password.")
-            return False
+            material, amount = next(iter(asteroid.materials.items()))
+            if amount <= 0:
+                self.laser.power_off()
+                print(f"\nAsteroid's {material} has been depleted.")
+                return
 
-        print("Logged in successfully.")
-        return True
+            time.sleep(self.laser.cycle_time)
+            available_space = self.cargo_capacity - total_cargo
+            mined_material = min(self.laser.power, amount, available_space)
+            mined_material = max(mined_material, 0)
+
+            self.current_cargo[material] = self.current_cargo.get(material, 0) + mined_material
+
+            asteroid.materials[material] -= mined_material
+
+            print(f"\rMined: {mined_material} units of {material}, Remaining in Asteroid: {asteroid.materials[material]}, "
+                f"Space Remaining in Cargo: {self.cargo_capacity - total_cargo}\n", end='', flush=True)
